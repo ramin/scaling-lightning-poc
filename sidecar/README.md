93bef3fff54d6daac2bf4ca1b2c69e08a5d637df
ramin <raminkeene@gmail.com>
2023-06-01 00:55:52 +0400

    hypothetical basic layout, including sidecar

---
+++ sidecar/README.md
@@ -0,0 +1,3 @@
+# sidecar
+
+poc that we can build and bundle a command sidecar that can receive requests to create interactions between running services
---
+++ sidecar/cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+// this service can implement a sidecar
+// which can be built and deployed as the command
+// listener to trigger interactions between
+// running nodes of any kind
+func main() {
+	fmt.Println("not at all working sidecar")
+}
